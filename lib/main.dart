03244ac79586921c7d4c46c45385335d74510bb8
k1ycee <chiagozieani145@gmail.com>
2021-06-22 15:53:46 +0100

    Flers

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:trade_app/helpers/locator.dart';
 import 'package:trade_app/screens/auth/email_verification.dart';
 import 'package:trade_app/screens/auth/forgot_password.dart';
@@ -10,35 +11,36 @@ import 'package:trade_app/screens/home.dart';
 
 void main() async {
   setupLocator();
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: LoginScreen.route,
-      routes: {
-        OnboardingScreen.route: (context) => OnboardingScreen(),
-        LoginScreen.route: (context) => LoginScreen(),
-        RegisterScreen.route: (context) => RegisterScreen(),
-        ForgotPassword.route: (context) => ForgotPassword(),
-        ResetPassword.route: (context) => ResetPassword(),
-        EmailVerification.route: (context) => EmailVerification(),
-        HomeScreen.route: (context) => HomeScreen(),
-      },
+    return ScreenUtilInit(
+      designSize: Size(MediaQuery.of(context).size.width,
+          MediaQuery.of(context).size.height),
+      builder: () => MaterialApp(
+        initialRoute: LoginScreen.route,
+        routes: {
+          OnboardingScreen.route: (context) => OnboardingScreen(),
+          LoginScreen.route: (context) => LoginScreen(),
+          RegisterScreen.route: (context) => RegisterScreen(),
+          ForgotPassword.route: (context) => ForgotPassword(),
+          ResetPassword.route: (context) => ResetPassword(),
+          EmailVerification.route: (context) => EmailVerification(),
+          HomeScreen.route: (context) => HomeScreen(),
+        },
+      ),
     );
   }
 }
-
-
 
 // import 'package:flutter_screenutil/flutter_screenutil.dart';
 // import 'package:trade_app/utils%20/locator.dart';
 // import 'package:trade_app/utils%20/navigator.dart';
 // import 'package:trade_app/utils%20/router.dart';
-
 
 // void main() {
 //   WidgetsFlutterBinding.ensureInitialized();
